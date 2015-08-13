8d501173adeb72d17a08a2ff05dbe859e7cc2b0a
Harrison <harrison@theshoebridges.com>
2015-08-14 08:47:40 +1000

    Add phaser instance

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+    console.log("preloading...")
+}
+
+function create() {
+    console.log("creating...")
+}
+
+function update() {
+    console.log("updating...")
+}
