eae54587b33efab0f6c3bdc6a265464647a0b22d
Harrison <harrison@theshoebridges.com>
2015-08-14 14:32:28 +1000

    Add controls muckup

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
 function preload() {
     console.log("preloading...")
