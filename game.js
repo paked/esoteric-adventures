51849f0d6141b7709b1a920191d91cdd3981f46d
Harrison <harrison@theshoebridges.com>
2015-08-22 09:04:05 +1000

    Add mobile scaling
    If the window size is bigger than 800x600, it will be set to a 800x400
    window, I chose this cause it *kind of* felt mobile-ish. @rafej feel
    free to change it to make it an actual phone size.

--- game.js
+++ game.js
@@ -1,4 +1,13 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
+var windowWidth = window.innerWidth * window.devicePixelRatio;
+var windowHeight = window.innerHeight * window.devicePixelRatio;
+var scaleRatio = window.devicePixelRatio / 3;
+
+if (windowWidth > 800 && windowHeight > 600) {
+    windowWidth = 800;
+    windowHeight = 400;
+}
+
+var game = new Phaser.Game(windowWidth, windowHeight, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update });
 
 function preload() {
     console.log("preloading...")
@@ -6,6 +15,11 @@ function preload() {
 
 function create() {
     game.stage.backgroundColor = '#72C257';
+
+    // NOTE:
+    // whenever you create a sprite or use some sort of asset
+    // remember to call: `MYASSET.scale.setTo(scaleRatio, scaleRatio);`
+    // This will scale it up to the correct resolution on mobile devices
 }
 
 function update() {
