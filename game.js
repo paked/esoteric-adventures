46013c7245f63f4512975eb3dbf13833feca331a
the-jellyfish <samuelrfinch@gmail.com>
2015-08-22 14:20:52 +1000

    Add basic player movement

--- game.js
+++ game.js
@@ -8,14 +8,25 @@ if (windowWidth > 800 && windowHeight > 600) {
 }
 
 var game = new Phaser.Game(windowWidth, windowHeight, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update });
-
+var player
+var cursors
 function preload() {
     console.log("preloading...")
+    game.load.image('player', 'player.png')
 }
 
 function create() {
-    game.stage.backgroundColor = '#72C257';
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.gravity.y = 250;
 
+
+    player = game.add.sprite(0, 0, 'player');
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+    player.body.bounce.y = 0.05;
+    player.body.collideWorldBounds = true;
+    
+    cursors = game.input.keyboard.createCursorKeys()
+    game.stage.backgroundColor = '#72C257';
     // NOTE:
     // whenever you create a sprite or use some sort of asset
     // remember to call: `MYASSET.scale.setTo(scaleRatio, scaleRatio);`
@@ -23,6 +34,15 @@ function create() {
 }
 
 function update() {
-    game.backgroundColor = '#ff0000';
-    console.log("updating...")
+    player.body.velocity = 0;
+
+   if (cursors.left.isDown){
+    player.body.velocity.x = -120;
+   }
+   if(cursors.right.isDown){
+    player.body.velocity.x = 120;
+   }
+
+
+ 
 }
