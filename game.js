fc5522bc359e2a65a3a19e6d1b6dfd094622a359
rafej <rafe.s@hotmail.com>
2015-08-22 14:45:35 +1000

    Added enemy movement (fuck yeah!)

--- game.js
+++ game.js
@@ -8,13 +8,21 @@ if (windowWidth > 800 && windowHeight > 600) {
 }
 
 var game = new Phaser.Game(windowWidth, windowHeight, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update });
-
+var enemy;
+var moveTime = 0;
 function preload() {
     console.log("preloading...")
+game.load.image('enemy','assets/enemy.png');
 }
 
 function create() {
-    game.stage.backgroundColor = '#72C257';
+game.stage.backgroundColor = '#72C257';
+game.physics.startSystem(Phaser.Physics.ARCADE);
+enemy = game.add.sprite(0,0,'enemy');
+game.physics.arcade.enable(enemy);
+enemy.body.velocity.x = 100;
+moveTime = game.time.now + 750;
+
 
     // NOTE:
     // whenever you create a sprite or use some sort of asset
@@ -25,4 +33,8 @@ function create() {
 function update() {
     game.backgroundColor = '#ff0000';
     console.log("updating...")
+if(game.time.now >= moveTime ){
+enemy.body.velocity.x *= -1;
+moveTime = game.time.now + 750;
+}
 }
